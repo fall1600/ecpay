7d8fd2356c034ef7a26b34aac42d6f2b77e39082
fall1600 <fall1600@gmail.com>
2020-05-11 13:29:16 +0800

    補點註解

--- Cryption.php
+++ Cryption.php
@@ -10,27 +10,40 @@ trait Cryption
 
     protected $hashIv;
 
+    /**
+     * 文件p.47
+     *   綠界說怎麼算就怎麼算
+     * @param  Info  $info
+     * @return string
+     */
     public function countCheckSum(Info $info)
     {
         $infoPayload = $info->getInfo();
 
+        // 參數依照字母排序
         ksort($infoPayload);
 
+        // 不能直接使用http_build_query, 否則中文會預先url encode 過
         $str = "HashKey=$this->hashKey";
         foreach ($infoPayload as $key => $value) {
             $str .= "&$key=$value";
         }
         $str .= "&HashIV=$this->hashIv";
 
+        // url encode
         $strEncoded = urlencode($str);
 
+        // 轉小寫
         $strLower = strtolower($strEncoded);
 
+        // 代換成 .Net 成接受的字元
         $strReplaced = $this->replaceSymbol($strLower);
 
-        $strMd5ed = hash('sha256', $strReplaced);
+        // 用sha256 hash
+        $strHashed = hash('sha256', $strReplaced);
 
-        return strtoupper($strMd5ed);
+        // 轉大寫
+        return strtoupper($strHashed);
     }
 
     protected function replaceSymbol(string $str)
