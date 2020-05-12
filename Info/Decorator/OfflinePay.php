5bc6f6507e3219c34fe7ab3d685d585ffacc3eee
fall1600 <fall1600@gmail.com>
2020-05-12 09:52:15 +0800

    offlinePay done

--- Info/Decorator/OfflinePay.php
+++ Info/Decorator/OfflinePay.php
@@ -10,13 +10,52 @@ class OfflinePay extends InfoDecorator
     /** @var Info */
     protected $info;
 
-    public function __construct(Info $info)
+    /**
+     * 離線付款取號完成, 通知系統的callback url
+     * @var string
+     */
+    protected $paymentInfoUrl;
+
+    /**
+     * 離線付款的有效繳費天數
+     *   綠界支援1~60天, 預設3天
+     * @var int
+     */
+    protected $ttl;
+
+    /**
+     * 取號完成後, 從綠界回導系統的網址
+     * @var string
+     */
+    protected $clientRedirectUrl;
+
+    public function __construct(Info $info, string $paymentInfoUrl, int $ttl = null, string $clientRedirectUrl = null)
     {
         $this->info = $info;
+
+        $this->paymentInfoUrl = $paymentInfoUrl;
+
+        $this->setTtl($ttl);
+
+        $this->clientRedirectUrl = $clientRedirectUrl;
     }
 
     public function getInfo()
     {
-        // TODO: Implement getInfo() method.
+        return $this->info->getInfo() +
+            [
+                'ExpireDate' => $this->ttl,
+                'PaymentInfoURL' => $this->paymentInfoUrl,
+                'ClientRedirectURL' => $this->clientRedirectUrl,
+            ];
+    }
+
+    protected function setTtl(int $ttl = null)
+    {
+        if ($ttl > 60 || $ttl < 1) {
+            throw new \LogicException('ttl only in 1~60 days');
+        }
+
+        $this->ttl = $ttl;
     }
 }
