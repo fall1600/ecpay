b7ed12273397e3f7327090b6505d4f7d1028f9fd
fall1600 <fall1600@gmail.com>
2020-05-13 14:47:36 +0800

    pay in period

--- Info/Decorator/PayInPeriods.php
+++ Info/Decorator/PayInPeriods.php
@@ -2,18 +2,117 @@
 
 namespace fall1600\Package\Ecpay\Info\Decorator;
 
+use fall1600\Package\Ecpay\Constants\PeriodType;
 use fall1600\Package\Ecpay\Info\Info;
 use fall1600\Package\Ecpay\Info\InfoDecorator;
 
 class PayInPeriods extends InfoDecorator
 {
-    public function __construct(Info $info, string $periodType)
-    {
+    /**
+     * @var Info
+     */
+    protected $info;
 
+    /**
+     * @var string
+     */
+    protected $periodType;
+
+    /**
+     * @var int
+     */
+    protected $frequency;
+
+    /**
+     * @var int
+     */
+    protected $times;
+
+    /**
+     * @var string
+     */
+    protected $periodReturnUrl;
+
+    public function __construct(Info $info, string $periodType, int $frequency, int $times, string $periodReturnUrl)
+    {
+        $this->info = $info;
+
+        $this->setPeriodType($periodType);
+
+        $this->setFrequency($frequency);
+
+        $this->setTimes($times);
+
+        $this->periodReturnUrl = $periodReturnUrl;
     }
 
     public function getInfo()
     {
-        // TODO: Implement getInfo() method.
+        return $this->info->getInfo() +
+            [
+                'PeriodAmount' => $this->info->getOrder()->getTotalAmount(),
+                'PeriodType' => $this->periodType,
+                'Frequency' => $this->frequency,
+                'ExecTimes' => $this->times,
+            ];
+    }
+
+    protected function setPeriodType(string $periodType)
+    {
+        if (! PeriodType::isValid($periodType)) {
+            throw new \LogicException('unsupported period type');
+        }
+
+        $this->periodType = $periodType;
+    }
+
+    protected function setFrequency(int $frequency)
+    {
+        switch ($this->periodType) {
+            case PeriodType::DAY:
+                if ($frequency < 1 || $frequency > 365) {
+                    throw new \LogicException('unsupported frequency in day');
+                }
+                break;
+            case PeriodType::MONTH:
+                if ($frequency < 1 || $frequency > 12) {
+                    throw new \LogicException('unsupported frequency in month');
+                }
+                break;
+            case PeriodType::YEAR:
+                if ($frequency != 1) {
+                    throw new \LogicException('unsupported frequency in year');
+                }
+                break;
+            default:
+                throw new \LogicException('unsupported frequency');
+        }
+
+        $this->frequency = $frequency;
+    }
+
+    protected function setTimes(int $times)
+    {
+        switch ($this->periodType) {
+            case PeriodType::DAY:
+                if ($times <= 1 || $times > 999) {
+                    throw new \LogicException('unsupported times in day');
+                }
+                break;
+            case PeriodType::MONTH:
+                if ($times <= 1 || $times > 99) {
+                    throw new \LogicException('unsupported times in month');
+                }
+                break;
+            case PeriodType::YEAR:
+                if ($times <= 1 || $times > 9) {
+                    throw new \LogicException('unsupported times in year');
+                }
+                break;
+            default:
+                throw new \LogicException('unsupported times');
+        }
+
+        $this->times = $times;
     }
 }
