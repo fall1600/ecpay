ffd56023afa1bebf50e2cd35d1f2322764e0b3e0
fall1600 <fall1600@gmail.com>
2020-05-16 01:31:31 +0800

    extraInfo

---
+++ Info/Decorator/ExtraInfo.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace fall1600\Package\Ecpay\Info\Decorator;
+
+use fall1600\Package\Ecpay\Info\Info;
+use fall1600\Package\Ecpay\Info\InfoDecorator;
+
+class ExtraInfo extends InfoDecorator
+{
+    /**
+     * @var Info
+     */
+    protected $info;
+
+    public function __construct(Info $info)
+    {
+        $this->info = $info;
+    }
+
+    /**
+     * 是否需要額外的付款資訊
+     * 注意事項: 額外回傳的參數全部都需要加入檢查碼計算
+     *
+     * @return array
+     */
+    public function getInfo()
+    {
+        return $this->info->getInfo() +
+            [
+                'NeedExtraPaidInfo' => 'Y'
+            ];
+    }
+}
